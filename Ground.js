dc5a9056ab1a3e40fbb7dab85085e6f4ff1012fe
lolcoder003 <74305446+lolcoder003@users.noreply.github.com>
2020-11-20 16:57:42 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,31 @@
+class Ground {
+  constructor(x,y,width,height){
+    var option={
+      isStatic : true 
+    }
+    
+    this.body = Bodies.rectangle(x,y,width,height,option);
+    this.height = height ;
+    this.width = width; 
+    World.add(world , this.body);
+  }
+
+  display(){
+    var pos = this.body.position;
+    rectMode(CENTER);
+    fill("cyan");
+    rect(pos.x, pos.y , this.width , this.height);
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
